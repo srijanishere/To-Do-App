0d4d7b8aaa0c436fb635708f9f03ad9e063e8bcd
srijanishere <banerjee.srijan2001@gmail.com>
2023-08-09 23:31:58 +0530

    Added backdrop filter

--- to_do_app/lib/screens/add_tasks_screen.dart
+++ to_do_app/lib/screens/add_tasks_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:slide_to_confirm/slide_to_confirm.dart';
 
@@ -9,57 +11,61 @@ class AddTasksScreen extends StatelessWidget {
     return Container(
       color: Color(
           0xff536a44), //takes the gray color of the background and helps in achieving the rounded border in the below container
-      child: Container(
-        padding: EdgeInsets.symmetric(
-          vertical: 30.0,
-          horizontal: 25.0,
-        ),
-        decoration: BoxDecoration(
-          color: Color(0xff6a9777).withOpacity(0.7),
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(20.0),
-            topRight: Radius.circular(20.0),
+      child: BackdropFilter(
+        filter: new ImageFilter.blur(sigmaX: 7.0, sigmaY: 7.0),
+        child: Container(
+          padding: EdgeInsets.symmetric(
+            vertical: 30.0,
+            horizontal: 25.0,
           ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              'Add Task',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 30.0,
-                fontWeight: FontWeight.w700,
-              ),
+          decoration: BoxDecoration(
+            // color: Color(0xff1d4c3d).withOpacity(0.7),
+            color: Colors.white.withOpacity(0.1),
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20.0),
+              topRight: Radius.circular(20.0),
             ),
-            TextField(
-              autofocus: true,
-              textAlign: TextAlign.center,
-              cursorColor: Colors.white,
-              decoration: InputDecoration(
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Add Task',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.w700,
                 ),
               ),
-              style: TextStyle(color: Colors.white),
-            ),
-            SizedBox(height: 20.0),
-            ConfirmationSlider(
-              onConfirmation: () {},
-              backgroundColor: Color(0xff5a5a5a),
-              sliderButtonContent: Icon(Icons.arrow_forward),
-              backgroundColorEnd: Color(0xffd0f388),
-              foregroundColor: Color(0xffa4fc7a),
-              text: 'Confirm',
-              textStyle: TextStyle(
-                color: Colors.white,
+              TextField(
+                autofocus: true,
+                textAlign: TextAlign.center,
+                cursorColor: Colors.white,
+                decoration: InputDecoration(
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                ),
+                style: TextStyle(color: Colors.white),
               ),
-            ),
-          ],
+              SizedBox(height: 20.0),
+              ConfirmationSlider(
+                onConfirmation: () {},
+                backgroundColor: Color(0xff5a5a5a),
+                sliderButtonContent: Icon(Icons.arrow_forward),
+                backgroundColorEnd: Color(0xffd0f388),
+                foregroundColor: Color(0xffa4fc7a),
+                text: 'Confirm',
+                textStyle: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
